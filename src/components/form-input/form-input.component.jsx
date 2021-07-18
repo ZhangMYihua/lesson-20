be77bf6220ec308ce1cfa6aa63f53497f6c9a39e
nguyenthanhhau-cqu <68142295+nguyenthanhhau-cqu@users.noreply.github.com>
2021-07-18 18:47:34 +1000

    Delete redudant onChange
    Since onChange pass as props in sign-up Form, there is no need of handle change

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './form-input.styles.scss';
 
-const FormInput = ({ handleChange, label, ...otherProps }) => (
+const FormInput = ({  label, ...otherProps }) => (
   <div className='group'>
-    <input className='form-input' onChange={handleChange} {...otherProps} />
+    <input className='form-input'  {...otherProps} />
     {label ? (
       <label
         className={`${
